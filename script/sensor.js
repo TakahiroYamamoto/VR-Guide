5a933fb28a7037659682c5933378e861878d4a1e
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-06 20:45:12 +0900

    use script

--- script/sensor.js
+++ script/sensor.js
@@ -1,6 +1,3 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0,
-maximum-scale=1.0, user-scalable=no" />
-<script type="text/javascript">
 window.addEventListener("deviceorientation", function(event) {
   var orientation = event.alpha;
   var pitch = event.beta;
