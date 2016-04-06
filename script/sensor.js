9eb246dc28e33e6f8abcdb246d4025f95ff946c5
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-07 00:48:00 +0900

    change direction by sensor2

--- script/sensor.js
+++ script/sensor.js
@@ -7,10 +7,6 @@ window.addEventListener("deviceorientation", function(event) {
   "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
   "ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
   "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
-  if(orientation.toFixed(1) < 180) {
-    $("absoluteMuki").css("transform", "rotate(90deg)");
-  }
-  else {
-    $("absoluteMuki").css("transform", "rotate(-90deg)");
-  }
+  $("absoluteMuki").css({transform:'rotate(' + orientation.toFixed(1) + 'deg)'});
+
 });
