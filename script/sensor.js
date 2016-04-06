c94ab2ea57e4d6c1deb11e2c527f084aa623f6ba
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-06 20:39:50 +0900

    use script

--- script/sensor.js
+++ script/sensor.js
@@ -1,10 +1,13 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0,
+maximum-scale=1.0, user-scalable=no" />
+<script type="text/javascript">
 window.addEventListener("deviceorientation", function(event) {
-var orientation = event.alpha;
-var pitch = event.beta;
-var roll= event.gamma;
-var result=document.getElementById("result");
-result.innerHTML = "方位(°)<br />"+
-"方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
-"ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
-"ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
+  var orientation = event.alpha;
+  var pitch = event.beta;
+  var roll= event.gamma;
+  var result=document.getElementById("result");
+  result.innerHTML = "方位(°)<br />"+
+  "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
+  "ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
+  "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
 });
