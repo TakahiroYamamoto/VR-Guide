63675e165152164b885cdff847f67573633a4592
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-08 02:57:32 +0900

    rotate 3D

--- script/sensor.js
+++ script/sensor.js
@@ -2,7 +2,7 @@ window.addEventListener("deviceorientation", function(event) {
   var orientation = event.alpha;
   var pitch = event.beta;
   var roll= event.gamma;
-  var margin = 90.0;
+  var margin = 0.0;
   var result=document.getElementById("result");
   result.innerHTML = "方位(°)<br />"+
   "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
