a1014b854912243a955c52cc0449786849738067
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-08 03:16:51 +0900

    rotate 3D

--- script/sensor.js
+++ script/sensor.js
@@ -6,7 +6,7 @@ window.addEventListener("deviceorientation", function(event) {
   var result=document.getElementById("result");
   result.innerHTML = "方位(°)<br />"+
   "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
-  "ピッチ（x軸回りの回転角度:-90～90）："+ (margin + pitch.toFixed(1)) +"<br />" +
+  "ピッチ（x軸回りの回転角度:-90～90）："+ (90 + pitch.toFixed(1)) +"<br />" +
   "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
   $(".absoluteHoui").css({transform:'rotate(' + (orientation.toFixed(1)) + 'deg)'});
   $(".relative").css({"-webkit-transform":'rotateX(' + (margin + pitch.toFixed(1)) + 'deg)'});
