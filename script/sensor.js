e64e575aa1f3eaaf59f091382d6b3b76216d1b15
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-10 16:25:55 +0900

    rotate 3D

--- script/sensor.js
+++ script/sensor.js
@@ -15,11 +15,39 @@ window.addEventListener("deviceorientation", function(event) {
 
 });
 
-var watchID = navigator.geolocation.watchPosition(function(position) {
+navigator.geolocation.getCurrentPosition(is_success,is_error);
+
+
+function is_success(position) {
+	var result = '結果';
+	result += '経度：' + position.coords.latitude + '<br>';
+	result += '緯度：' + position.coords.longitude + '<br>';
+	result += '高度：' + position.coords.attitude + '<br>';
+	result += '経度・緯度の誤差：' + position.coords.accuracy + '<br>';
+	result += '高度の誤差：' + position.coords.altitudeAccuracy + '<br>';
+	result += '方角：' + position.coords.heading + '<br>';
+	result += '速度：' + position.coords.speed + '<br>';
+
+	document.getElementById('result2').innerHTML = result;
   var myLatitude = 35.55475412985779 - position.coords.latitude;
   var myLongitude = 139.67818632651213 - position.coords.longitude;
   double radian = Math.atan2(myLatitude,myLongitude);
   double degree = (radian * 180d / Math.PI);
   $(".absoluteMuki").css({transform:'rotate(' + degree + 'deg)'});
+}
 
-});
+function is_error(error) {
+	var result = "";
+	switch(error.code) {
+		case 1:
+			result = '位置情報の取得が許可されていません';
+		break;
+		case 2:
+			result = '位置情報の取得に失敗しました';
+		break;
+		case 3:
+			result = 'タイムアウトしました';
+		break;
+	}
+	document.getElementById('result2').innerHTML = result;
+}
