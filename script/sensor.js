cc28a1dab2d639862c407e7f729dbe8cd81bf21c
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-10 16:41:21 +0900

    rotate 3D

--- script/sensor.js
+++ script/sensor.js
@@ -1,5 +1,20 @@
 
 
+window.addEventListener("deviceorientation", function(event) {
+  var orientation = event.webkitCompassHeading;
+  var pitch = event.beta;
+  var roll= event.gamma;
+  var iRoll = 90 + parseInt(pitch.toFixed(1));
+  var result=document.getElementById("result");
+  result.innerHTML = "方位(°)<br />"+
+  "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
+  "ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
+  "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
+  $(".absoluteHoui").css({transform:'rotate(' + (orientation.toFixed(1)) + 'deg)'});
+  $(".relative").css({"-webkit-transform":'rotateX(' + (iRoll) + 'deg)'});
+
+});
+
 navigator.geolocation.getCurrentPosition(is_success,is_error);
 
 
@@ -14,6 +29,7 @@ function is_success(position) {
 	result += '速度：' + position.coords.speed + '<br>';
 
 	document.getElementById('result2').innerHTML = result;
+
 }
 
 function is_error(error) {
