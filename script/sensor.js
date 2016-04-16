532c5a29c1664fab8338484541c489eec1cba79d
TakahiroYamamoto <hogazurou_0225@yahoo.co.jp>
2016-04-16 22:56:11 +0900

    向き再計算

--- script/sensor.js
+++ script/sensor.js
@@ -100,9 +100,22 @@ function calc_distance(lat_1, lng_1, lat_2, lng_2) {
   return d;
 }
 
+function geoDirection(lat1, lng1, lat2, lng2) {
+  // 緯度経度 lat1, lng1 の点を出発として、緯度経度 lat2, lng2 への方位
+  // 北を０度で右回りの角度０～３６０度
+  var Y = Math.cos(lng2 * Math.PI / 180) * Math.sin(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+  var X = Math.cos(lng1 * Math.PI / 180) * Math.sin(lng2 * Math.PI / 180) - Math.sin(lng1 * Math.PI / 180) * Math.cos(lng2 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+  var dirE0 = 180 * Math.atan2(Y, X) / Math.PI; // 東向きが０度の方向
+  if (dirE0 < 0) {
+    dirE0 = dirE0 + 360; //0～360 にする。
+  }
+  var dirN0 = (dirE0 + 90) % 360; //(dirE0+90)÷360の余りを出力 北向きが０度の方向
+  return dirN0;
+}
+
 function updateValue(){
-  var radian = Math.atan2(g_myLongitude-g_Longitude,g_myLatitude-g_Latitude);
-  g_degree = (radian * 180.0 / Math.PI);
+  var radian = Math.atan2(g_mylatitude,g_myLongitude,g_Latitude,g_Longitude);
+  g_degree = geoDirection()
   document.getElementById('distanceText').innerHTML = calc_distance(g_Latitude,g_Longitude,g_myLatitude,g_myLongitude);
   $(".absoluteText1").html(calc_distance(g_Latitude,g_Longitude,g_myLatitude,g_myLongitude));
 }
